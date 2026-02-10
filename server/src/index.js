5e46ed6e3e35cd4fbdf6c29e086b99f38cda27db
khushthecoder <chaudharikhush0711@gmail.com>
2026-02-10 15:35:00 +0530

    bootstrap application entrypoint

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const app = require('./app');
+const { logger } = require('./utils/logger');
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  logger.info(`Veloura server listening on http://localhost:${PORT}`);
+});
