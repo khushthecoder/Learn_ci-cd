d14ea48b343401d21644e3ba7816f70b43d39779
khushthecoder <chaudharikhush0711@gmail.com>
2026-02-10 12:50:00 +0530

    setup request logger

---
+++ server/src/middleware/requestLogger.js
@@ -0,0 +1,20 @@
+const { logger } = require('../utils/logger');
+
+function requestLogger(req, res, next) {
+  const start = Date.now();
+  res.on('finish', () => {
+    const ms = Date.now() - start;
+    logger.info(`${req.method} ${req.originalUrl} ${res.statusCode} ${ms}ms`);
+  });
+  next();
+}
+
+module.exports = { requestLogger };
+
+// minor update
+
+// minor update
+
+// minor update
+
+// minor update
