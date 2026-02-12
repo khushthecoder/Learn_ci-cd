a14c8608f0f1850481a09bbef208201823bcf8cf
khushthecoder <chaudharikhush0711@gmail.com>
2026-02-12 12:26:00 +0530

    setup prisma

---
+++ server/src/lib/prisma.js
@@ -0,0 +1,17 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = global.__prisma || new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  global.__prisma = prisma;
+}
+
+module.exports = prisma;
+
+// minor update
+
+// minor update
+
+// minor update
