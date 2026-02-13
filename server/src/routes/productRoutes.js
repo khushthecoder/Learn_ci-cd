55130a746b5729f30c33d5081093231ecce9aa03
khushthecoder <chaudharikhush0711@gmail.com>
2026-02-13 15:50:00 +0530

    add api routes for product routes

---
+++ server/src/routes/productRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {
+  listProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require('../controllers/productController');
+
+const router = express.Router();
+
+router.get('/', listProducts);
+router.get('/:id', getProduct);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
+module.exports = router;
+
+// minor update
+
+// minor update
